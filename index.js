cc7eec311fd899ad90f35bf6ea053a83f125c6ff
Adosio14 <dosioalvaromartin@gmail.com>
2023-04-19 20:11:32 -0300

    working feedback field

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-
-function subirComentario() {
-    console.log("Holis")
-    document.getElementById("boton").addEventListener("click")
-    var nombre = document.getElementById("nombre").value;
-    var comentario = document.getElementById("comentario").value;
-    var nuevoComentario = {nombre: nombre, comentario: comentario};
-    var li = document.createElement("li");
-    li.appendChild(nuevoComentario.comentario)
-    var div = document.getElementById("comentarios")
-    div.appendChild(li)
-    document.getElementById("nombre").value = "";
-    document.getElementById("comentario").value = "";
+let button = document.getElementById("boton")
+button.addEventListener("click", subirComentario)
+function subirComentario(evento) {
+    evento.preventDefault();
+    var comentario = document.getElementById("cajacomentarios").value;
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(comentario))
+    let ul = document.getElementById("comentariosul")
+    ul.appendChild(li)
+    document.getElementById("cajacomentarios").value = "";
 }
