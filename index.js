bfcb6384e4033123da5c9570cf9f339f0ed0c3df
Adosio14 <dosioalvaromartin@gmail.com>
2023-05-10 20:26:09 -0300

    Separating functions

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+let productCardStyle = ""
 let productsDiv = document.getElementById("productsContainer")
+
 getProducts()
 function subirComentario(evento) {
     evento.preventDefault();
@@ -13,41 +15,46 @@ function subirComentario(evento) {
 
 async function getProducts(){
     let products = [];
-    let productsContainer = document.getElementById("productsContainer")
     fetch("https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products").then((response)=>{
         response.json().then((productsJson)=>{
-            products = productsJson
-            products.forEach(element => {
-                let types = element.notebooksTypes
-                let productCard = document.createElement("div")
-                productCard.classList.add("productCard");
-                let productTitle =  document.createElement("h4")
-                productTitle.textContent = element.title
-                let link = document.createElement("a")
-                link.href = "https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/" + element.id
-                link.target = "_blank"
-                link.appendChild(productTitle)
-                let productImage =  document.createElement("img");
-                productImage.src = element.image_url 
-                productImage.alt = element.title
-                productImage.id = "productImage"
-                let productDescription = document.createElement("p")
-                productDescription.textContent = element.description
-                productCard.appendChild(link)
-                productCard.appendChild(productImage)
-                productCard.appendChild(productDescription)
-                types.forEach(type =>{
-                    let productVariant = document.createElement("P")
-                    let productPrice = document.createElement("p")
-                    let productRam = document.createElement("strong")
-                    productRam.textContent = type.ramAmount + " "
-                    productPrice.textContent = "$ " + type.price
-                    productVariant.appendChild(productRam)
-                    productVariant.appendChild(productPrice)
-                    productCard.appendChild(productVariant)
-                })
-                productsContainer.appendChild(productCard)
-            });
+            createElement(productsJson)
         })
     });
+}
+async function createElement(products){
+    
+    products.forEach(element => {
+        let types = element.notebooksTypes
+        let productCard = document.createElement("div")
+        productCard.setAttribute('class', 'col-xl-4 col-lg-6 col-md-6 col-sm-12 p-4 h-100')
+        let productTitle =  document.createElement("h4")
+        productTitle.textContent = element.title
+        let link = document.createElement("a")
+        link.href = "https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/" + element.id
+        link.target = "_blank"
+        link.appendChild(productTitle)
+        let productImage =  document.createElement("img");
+        productImage.src = element.image_url 
+        productImage.alt = element.title
+        let productDescription = document.createElement("p")
+        productDescription.textContent = element.description
+        productCard.appendChild(link)
+        productCard.appendChild(productImage)
+        productCard.appendChild(productDescription)
+        types.forEach(type =>{
+            let productVariant = document.createElement("P")
+            let productPrice = document.createElement("p")
+            let productRam = document.createElement("strong")
+            productRam.textContent = type.ramAmount + " "
+            productPrice.textContent = "$ " + type.price
+            productVariant.appendChild(productRam)
+            productVariant.appendChild(productPrice)
+            productCard.appendChild(productVariant)
+        })
+        addElements(productCard)
+    });
+}
+async function addElements(productCard){
+    let productsContainer = document.getElementById("productsContainer")
+    productsContainer.appendChild(productCard)
 }
